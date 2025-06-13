2648a623ee0403d5841bf02ed7e1b069f7e64af5
Ruan <oliveiraruan2018@gmail.com>
2025-06-12 23:47:46 -0400

    feat: addjusting in logic of revoked and update consent

--- src/app/controllers/OpenFinanceController.js
+++ src/app/controllers/OpenFinanceController.js
@@ -1,20 +1,20 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
-import Account from "../models/Account.js";
-import Institution from "../models/Institution.js";
-import User from "../models/User.js";
-import OpenFinance from "../models/OpenFinance.js";
-import Transaction from "../models/Transaction.js";
+import Account from '../models/Account.js';
+import Institution from '../models/Institution.js';
+import User from '../models/User.js';
+import OpenFinance from '../models/OpenFinance.js';
+import Transaction from '../models/Transaction.js';
 
 class OpenFinanceController {
   async store(req, res) {
     try {
       const schema = Yup.object().shape({
         cpf: Yup.string()
-          .required("O campo CPF é obrigatório.")
+          .required('O campo CPF é obrigatório.')
           .max(
             11,
-            "Por favor, digite somente os números sem pontuação. Exemplo: 00000000000"
+            'Por favor, digite somente os números sem pontuação. Exemplo: 00000000000',
           ),
         // .test(
         //   "valid-cpf",
@@ -26,24 +26,24 @@ class OpenFinanceController {
         //   }
         // )
         expirationDate: Yup.date()
-          .when("expiration", {
+          .when('expiration', {
             is: true,
             then: (schema) =>
-              schema.required("A data de expiração é obrigatória."),
+              schema.required('A data de expiração é obrigatória.'),
             otherwise: (schema) => schema.notRequired(),
           })
-          .min(new Date(), "A data de expiração deve ser futura."),
+          .min(new Date(), 'A data de expiração deve ser futura.'),
         expiration: Yup.boolean()
-          .required("O campo de expiração é obrigatório.")
+          .required('O campo de expiração é obrigatório.')
           .oneOf(
             [true, false],
-            "O campo de expiração deve ser verdadeiro ou falso."
+            'O campo de expiração deve ser verdadeiro ou falso.',
           ),
         authorization: Yup.boolean()
-          .required("O campo de autorização é obrigatório.")
+          .required('O campo de autorização é obrigatório.')
           .oneOf(
             [true, false],
-            "O campo de autorização deve ser verdadeiro ou falso."
+            'O campo de autorização deve ser verdadeiro ou falso.',
           ),
       });
       const validatedData = await schema.validate(req.body, {
@@ -56,22 +56,22 @@ class OpenFinanceController {
         include: [
           {
             model: Account,
-            as: "accounts",
+            as: 'accounts',
             include: [
               {
                 model: Institution,
-                as: "institution",
+                as: 'institution',
               },
             ],
           },
         ],
       });
       if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado" });
+        return res.status(404).json({ error: 'Usuário não encontrado' });
       }
       if (!user?.accounts || user?.accounts?.length === 0) {
         return res.status(404).json({
-          error: "Usuário não possui contas cadastradas",
+          error: 'Usuário não possui contas cadastradas',
         });
       }
       const account = user.accounts[0];
@@ -91,7 +91,7 @@ class OpenFinanceController {
 
         return res.status(200).json({
           success: true,
-          message: "Compartilhamento atualizado com sucesso",
+          message: 'Compartilhamento atualizado com sucesso',
           data: {
             account: {
               institutionName: account.institution.name,
@@ -108,7 +108,7 @@ class OpenFinanceController {
       });
       return res.status(201).json({
         success: true,
-        message: "Compartilhamento feito com sucesso",
+        message: 'Compartilhamento feito com sucesso',
         data: {
           account: {
             institutionName: account.institution.name,
@@ -120,7 +120,7 @@ class OpenFinanceController {
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         return res.status(422).json({
-          error: "Erro na validação",
+          error: 'Erro na validação',
           messages: error.inner.map((error) => ({
             field: error.path,
             message: error.message,
@@ -128,7 +128,7 @@ class OpenFinanceController {
         });
       }
       return res.status(500).json({
-        error: "Erro interno do servidor",
+        error: 'Erro interno do servidor',
         details: error?.message ? error?.message : error,
       });
     }
@@ -140,45 +140,45 @@ class OpenFinanceController {
       if (!account || !agency) {
         return res
           .status(400)
-          .json({ error: "A conta e agência são obrigatórios" });
+          .json({ error: 'A conta e agência são obrigatórios' });
       }
       const accountData = await Account.findOne({
         where: { account, agency },
         include: [
           {
             model: Institution,
-            as: "institution",
-            attributes: ["id", "name"],
+            as: 'institution',
+            attributes: ['id', 'name'],
           },
           {
             model: User,
-            as: "user",
-            attributes: ["id", "name", "email"],
+            as: 'user',
+            attributes: ['id', 'name', 'email'],
           },
         ],
       });
       if (!accountData) {
-        return res.status(404).json({ error: "Conta não encontrada" });
+        return res.status(404).json({ error: 'Conta não encontrada' });
       }
       const openFinanceData = await OpenFinance.findOne({
         where: { account_id: accountData.id, status: true },
-        attributes: ["id", "account_id", "status", "expiration_date"],
+        attributes: ['id', 'account_id', 'status', 'expiration_date'],
       });
       if (!openFinanceData) {
         return res
           .status(404)
-          .json({ error: "Compartilhamento não encontrado" });
+          .json({ error: 'Compartilhamento não encontrado' });
       }
       if (!openFinanceData.status) {
         return res
           .status(403)
-          .json({ error: "Compartilhamento não autorizado" });
+          .json({ error: 'Compartilhamento não autorizado' });
       }
       if (
         openFinanceData.expiration_date &&
         new Date() > new Date(openFinanceData.expiration_date)
       ) {
-        return res.status(403).json({ error: "Compartilhamento expirado" });
+        return res.status(403).json({ error: 'Compartilhamento expirado' });
       }
       return res.json({
         success: true,
@@ -188,7 +188,7 @@ class OpenFinanceController {
       });
     } catch (error) {
       return res.status(500).json({
-        error: "Erro ao listar compartilhamento",
+        error: 'Erro ao listar compartilhamento',
         details: error?.message ? error?.message : error,
       });
     }
@@ -199,10 +199,10 @@ class OpenFinanceController {
       const { action } = req.params;
       const schema = Yup.object().shape({
         cpf: Yup.string()
-          .required("O campo CPF é obrigatório.")
+          .required('O campo CPF é obrigatório.')
           .max(
             11,
-            "Por favor, digite somente os números sem pontuação. Exemplo: 00000000000"
+            'Por favor, digite somente os números sem pontuação. Exemplo: 00000000000',
           ),
         // .test(
         //   "valid-cpf",
@@ -214,29 +214,29 @@ class OpenFinanceController {
         //   }
         // )
         expirationDate: Yup.date()
-          .when("expiration", {
+          .when('expiration', {
             is: true,
             then: (schema) =>
-              schema.required("A data de expiração é obrigatória."),
+              schema.required('A data de expiração é obrigatória.'),
             otherwise: (schema) => schema.notRequired(),
           })
-          .min(new Date(), "A data de expiração deve ser futura."),
+          .min(new Date(), 'A data de expiração deve ser futura.'),
         expiration: Yup.boolean()
-          .when("authorization", {
+          .when('authorization', {
             is: true,
             then: (schema) =>
-              schema.required("O campo de expiração é obrigatório."),
+              schema.required('O campo de expiração é obrigatório.'),
             otherwise: (schema) => schema.notRequired(),
           })
           .oneOf(
             [true, false],
-            "O campo de expiração deve ser verdadeiro ou falso."
+            'O campo de expiração deve ser verdadeiro ou falso.',
           ),
         authorization: Yup.boolean()
-          .required("O campo de autorização é obrigatório.")
+          .required('O campo de autorização é obrigatório.')
           .oneOf(
             [true, false],
-            "O campo de autorização deve ser verdadeiro ou falso."
+            'O campo de autorização deve ser verdadeiro ou falso.',
           ),
       });
       const validatedData = await schema.validate(req.body, {
@@ -249,40 +249,40 @@ class OpenFinanceController {
         include: [
           {
             model: Account,
-            as: "accounts",
+            as: 'accounts',
             include: [
               {
                 model: Institution,
-                as: "institution",
+                as: 'institution',
               },
             ],
           },
         ],
       });
       if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado" });
+        return res.status(404).json({ error: 'Usuário não encontrado' });
       }
       if (!user?.accounts || user?.accounts?.length === 0) {
         return res.status(404).json({
-          error: "Usuário não possui contas cadastradas",
+          error: 'Usuário não possui contas cadastradas',
         });
       }
       const account = user.accounts[0];
       const openFinance = await OpenFinance.findOne({
-        where: { account_id: account.id, status: true },
+        where: { account_id: account.id },
       });
       if (!openFinance) {
         return res
           .status(404)
-          .json({ error: "Compartilhamento não encontrado" });
+          .json({ error: 'Compartilhamento não encontrado' });
       }
-      if (action === "update") {
+      if (action === 'update') {
         openFinance.status = authorization;
         openFinance.expiration_date = expiration ? expirationDate : null;
         await openFinance.save();
         return res.json({
           success: true,
-          message: "Autorização atualizado com sucesso",
+          message: 'Autorização atualizado com sucesso',
           data: {
             account: {
               institutionName: account.institution.name,
@@ -292,19 +292,19 @@ class OpenFinanceController {
           },
         });
       }
-      if (action === "revoke") {
+      if (action === 'revoke') {
         openFinance.status = false;
         openFinance.expiration_date = null;
         await openFinance.save();
         return res.json({
           success: true,
-          message: "Autorização revogado com sucesso",
+          message: 'Autorização revogado com sucesso',
         });
       }
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         return res.status(422).json({
-          error: "Erro na validação",
+          error: 'Erro na validação',
           messages: error.inner.map((error) => ({
             field: error.path,
             message: error.message,
@@ -312,7 +312,7 @@ class OpenFinanceController {
         });
       }
       return res.status(500).json({
-        error: "Erro ao atualizar compartilhamento",
+        error: 'Erro ao atualizar compartilhamento',
         details: error?.message ? error?.message : error,
       });
     }
@@ -322,15 +322,15 @@ class OpenFinanceController {
     try {
       const schema = Yup.object().shape({
         account: Yup.string()
-          .required("O campo conta é obrigatório.")
-          .max(11, "A conta deve ter no máximo 11 caracteres."),
+          .required('O campo conta é obrigatório.')
+          .max(11, 'A conta deve ter no máximo 11 caracteres.'),
         agency: Yup.string()
-          .required("O campo agência é obrigatório.")
-          .max(4, "A agência deve ter no máximo 4 caracteres."),
+          .required('O campo agência é obrigatório.')
+          .max(4, 'A agência deve ter no máximo 4 caracteres.'),
         amount: Yup.number()
-          .required("O campo valor é obrigatório.")
-          .min(0.01, "O valor deve ser maior que zero.")
-          .typeError("O valor deve ser um número."),
+          .required('O campo valor é obrigatório.')
+          .min(0.01, 'O valor deve ser maior que zero.')
+          .typeError('O valor deve ser um número.'),
       });
       const validatedData = await schema.validate(req.body, {
         abortEarly: false,
@@ -341,18 +341,18 @@ class OpenFinanceController {
         include: [
           {
             model: Institution,
-            as: "institution",
-            attributes: ["id", "name"],
+            as: 'institution',
+            attributes: ['id', 'name'],
           },
           {
             model: User,
-            as: "user",
-            attributes: ["id", "name", "email"],
+            as: 'user',
+            attributes: ['id', 'name', 'email'],
           },
         ],
       });
       if (!accountData) {
-        return res.status(404).json({ error: "Conta não encontrada" });
+        return res.status(404).json({ error: 'Conta não encontrada' });
       }
       const openFinanceData = await OpenFinance.findOne({
         where: { account_id: accountData.id },
@@ -360,28 +360,28 @@ class OpenFinanceController {
       if (!openFinanceData) {
         return res
           .status(404)
-          .json({ error: "Compartilhamento não encontrado" });
+          .json({ error: 'Compartilhamento não encontrado' });
       }
-      if (openFinanceData.status) {
+      if (!openFinanceData.status) {
         return res
           .status(403)
-          .json({ error: "Compartilhamento não autorizado" });
+          .json({ error: 'Compartilhamento não autorizado' });
       }
       if (
         openFinanceData.expiration_date &&
         new Date() > new Date(openFinanceData.expiration_date)
       ) {
-        return res.status(403).json({ error: "Compartilhamento expirado" });
+        return res.status(403).json({ error: 'Compartilhamento expirado' });
       }
       if (accountData.balance < amount) {
         return res
           .status(422)
-          .json({ error: "Saldo insuficiente para realizar a transação" });
+          .json({ error: 'Saldo insuficiente para realizar a transação' });
       }
       const transaction = await Transaction.create({
         account_id: accountData.id,
         value: amount,
-        type: "debit",
+        type: 'debit',
       });
       const parsedBalance = parseFloat(accountData.balance);
       const parsedValue = parseFloat(amount);
@@ -389,7 +389,7 @@ class OpenFinanceController {
       await accountData.update({ balance: newBalance });
       return res.json({
         success: true,
-        message: "Transação feita com sucesso",
+        message: 'Transação feita com sucesso',
         data: {
           balance: accountData.balance,
         },
@@ -397,7 +397,7 @@ class OpenFinanceController {
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         return res.status(422).json({
-          error: "Erro na validação",
+          error: 'Erro na validação',
           messages: error.inner.map((error) => ({
             field: error.path,
             message: error.message,
@@ -405,7 +405,7 @@ class OpenFinanceController {
         });
       }
       return res.status(500).json({
-        error: "Erro ao registrar transação",
+        error: 'Erro ao registrar transação',
         details: error?.message ? error?.message : error,
       });
     }
